d8ad52f1bb8fda7dd08985d4ced6102a06ebcd8c
Justin VanWinkle <justinvanwinkle@knights.ucf.edu>
2015-03-30 10:48:05 -0400

    week 10 & duelingPhil as submitted

--- CS2ProgrammingWeek10/src/CS2ProgrammingWeek10.java
+++ CS2ProgrammingWeek10/src/CS2ProgrammingWeek10.java
@@ -33,9 +33,9 @@ public class CS2ProgrammingWeek10
 	//	A single value has a span of 1. Returns the largest span found in the 
 	//	given array. (Efficiency is not a priority.) 
 
-	//	maximumSpan({1, 2, 1, 1, 3}) → 4
-	//	maximumSpan({1, 4, 2, 1, 4, 1, 4}) → 6
-	//	maximumSpan({1, 4, 2, 1, 4, 4, 4}) → 6
+	//	maximumSpan({1, 2, 1, 1, 3}) == 4
+	//	maximumSpan({1, 4, 2, 1, 4, 1, 4}) == 6
+	//	maximumSpan({1, 4, 2, 1, 4, 4, 4}) == 6
 	
 	/**
 	 * 
@@ -57,11 +57,12 @@ public class CS2ProgrammingWeek10
 				{
 					if(nLargestSpan < j-i+1)
 					{
-						nLargetstSpan = j-i+1;
+						nLargestSpan = j-i+1;
 					}
 				}
 			}
 		}
+		return nLargestSpan;
 	}
 
 	//	Problem #2
@@ -69,9 +70,9 @@ public class CS2ProgrammingWeek10
 	//	array so that the sum of the numbers on one side is equal to the sum of 
 	//	the numbers on the other side. 
 
-	//	canStabilize({1, 1, 1, 2, 1}) → true
-	//	canStabilize({2, 1, 1, 2, 1}) → false
-	//	canStabilize({10, 10}) → true
+	//	canStabilize({1, 1, 1, 2, 1}) == true
+	//	canStabilize({2, 1, 1, 2, 1}) == false
+	//	canStabilize({10, 10}) == true
 	
 	/** 
 	 * 
@@ -88,18 +89,18 @@ public class CS2ProgrammingWeek10
 		
 		if(nums.length < 2)
 			return false;
-		
+		// get sum
 		for(int i=0; i<nums.length; i++)
 		{
-			for(int left=0; left<i; left++)
-			{
-				nSumLeft += nums[left];
-			}
-			for(int right=i; right<nums.length; right++)
-			{
-				nSumRight += nums[right];
-			}
-			
+			nSumRight += nums[i];
+		}
+		
+		// walk right, adding to left and taking from right
+		for(int i=0; i<nums.length; i++)
+		{
+			nSumLeft += nums[i];
+			nSumRight -= nums[i];
+						
 			if(nSumLeft == nSumRight)
 				return true;
 			else continue;
@@ -112,9 +113,9 @@ public class CS2ProgrammingWeek10
 	//	(spaces added to show the grouping). Note that the length of the array will be 
 	//	1 + 2 + 3 ... + n, which is known to sum to exactly n*(n + 1)/2. 
 
-	//	arithmeticSeries(3) → {1, 1, 2, 1, 2, 3}
-	//	arithmeticSeries(4) → {1, 1, 2, 1, 2, 3, 1, 2, 3, 4}
-	//	arithmeticSeries(2) → {1, 1, 2}
+	//	arithmeticSeries(3) == {1, 1, 2, 1, 2, 3}
+	//	arithmeticSeries(4) == {1, 1, 2, 1, 2, 3, 1, 2, 3, 4}
+	//	arithmeticSeries(2) == {1, 1, 2}
 	
 	/**
 	 * 
@@ -126,15 +127,25 @@ public class CS2ProgrammingWeek10
 	 */
 	static int[] arithmeticSeries(int n) 
 	{
-		int[] array = new Integer[];
+		int[] array = new int[n*(n+1)/2];
+		int tracker = 0;
+		int curNum = 1;
 		
-		for(int i=0; i<n; i++)
+		if(n == 0)
 		{
-			for(int j=0; j<i; j++)
-			{
-				array[j] = j+1;
-			}
+			return array;
 		}
+		
+		for(int i=1; i<=n; i++)
+		{
+			for(int j=tracker; curNum<=i; j++)
+			{
+				array[j] = curNum++;
+				tracker++;
+			}
+			curNum = 1;
+		}
+		return array;
 	}	
 
 	//	Problem #4
@@ -144,9 +155,9 @@ public class CS2ProgrammingWeek10
 	//	3's and 4's, every 3 has a number after it that is not a 3 or 4, and a 3 appears 
 	//	in the array before any 4. 
 
-	//	follow3with4({1, 3, 1, 4}) → {1, 3, 4, 1}
-	//	follow3with4({1, 3, 1, 4, 4, 3, 1}) → {1, 3, 4, 1, 1, 3, 4}
-	//	follow3with4({3, 2, 2, 4}) → {3, 4, 2, 2}
+	//	follow3with4({1, 3, 1, 4}) == {1, 3, 4, 1}
+	//	follow3with4({1, 3, 1, 4, 4, 3, 1}) == {1, 3, 4, 1, 1, 3, 4}
+	//	follow3with4({3, 2, 2, 4}) == {3, 4, 2, 2}
 	
 	/**
 	 * 
@@ -158,7 +169,8 @@ public class CS2ProgrammingWeek10
 	 */
 	static int[] follow3with4(int[] nums) 
 	{
-		int[] returnArray = new Integer[nums.length];
+		int j=0;
+		int[] returnArray = new int[nums.length];
 		
 		// copy array
 		for(int i=0; i<nums.length; i++)
@@ -166,19 +178,40 @@ public class CS2ProgrammingWeek10
 			returnArray[i] = nums[i];
 		}
 		
-		// swap 4s next to 3s
 		for(int i=0; i<nums.length; i++)
 		{
-			if(returnArray[i] == 3)
+			// find next 4
+			if(returnArray[i] == 3 && returnArray[i+1] != 4)
 			{
-				// find next 4 and swap it with i+1
-				for(int j=i+1; j<nums.length; j++)
+				for(int k=j; k<nums.length; k++)
 				{
-					if(returnArray[j] == 4)
+					// find next 5
+					if(returnArray[k] == 4)
 					{
-						returnArray[j] = returnArray[i+1];
-						returnArray[i+1] = 4;
-						break;
+						// there are no 5s before this
+						j=k+1;
+						// if a 5 is placed, don't touch this 5
+						if(k > 0)
+						{
+							if(returnArray[k-1] == 3)
+							{
+								
+								continue;
+							}
+							else
+							{
+								returnArray[k] = returnArray[i+1];
+								returnArray[i+1] = 4;
+								break;
+							}
+						}
+					
+						else
+						{
+							returnArray[k] = returnArray[i+1];
+							returnArray[i+1] = 4;
+							break;
+						}
 					}
 				}
 			}
@@ -192,9 +225,9 @@ public class CS2ProgrammingWeek10
 	//	solution makes only a single "linear" pass of both arrays, taking 
 	//	advantage of the fact that both arrays are already in sorted order. 
 
-	//	innerAppearsInOuter({1, 2, 4, 6}, {2, 4}) → true
-	//	innerAppearsInOuter({1, 2, 4, 6}, {2, 3, 4}) → false
-	//	innerAppearsInOuter({1, 2, 4, 4, 6}, {2, 4}) → true
+	//	innerAppearsInOuter({1, 2, 4, 6}, {2, 4}) == true
+	//	innerAppearsInOuter({1, 2, 4, 6}, {2, 3, 4}) == false
+	//	innerAppearsInOuter({1, 2, 4, 4, 6}, {2, 4}) == true
 	
 	/**
 	 * 
@@ -210,7 +243,7 @@ public class CS2ProgrammingWeek10
 	{
 		int i = 0;
 		
-		for(int j=0; i<inner.length; i++)
+		for(int j=0; j<outer.length; j++)
 		{
 			if(outer[j] == inner[i])
 			{
@@ -231,9 +264,9 @@ public class CS2ProgrammingWeek10
 	//	example, the largest mirror section in {1, 2, 3, 8, 9, 3, 2, 1} is length 
 	//	3 (the {1, 2, 3} part). Return the size of the largest mirror section found in the given array. 
 
-	//	maximumMirrorSpan({1, 2, 3, 8, 9, 3, 2, 1}) → 3
-	//	maximumMirrorSpan({1, 2, 1, 4}) → 3
-	//	maximumMirrorSpan({7, 1, 2, 9, 7, 2, 1}) → 2
+	//	maximumMirrorSpan({1, 2, 3, 8, 9, 3, 2, 1}) == 3
+	//	maximumMirrorSpan({1, 2, 1, 4}) == 3
+	//	maximumMirrorSpan({7, 1, 2, 9, 7, 2, 1}) == 2
 	
 	/**
 	 * 
@@ -250,23 +283,30 @@ public class CS2ProgrammingWeek10
 		
 		for(int i=0; i<nums.length; i++)
 		{
-			for(int j=nums.length-1; j>=0; j++)
+			for(int j=nums.length-1; j>=0; j--)
 			{
 				if(nums[i] == nums[j])
 				{
 					curSpan++;
 					i++;
-					if(i > nums.length\2)
+					if(i >= nums.length)
 						break;
 				}
 				else
 					if(curSpan > maxSpan)
 					{
 						maxSpan = curSpan;
+						curSpan = 0;
 					}
-				curSpan = 0;
+					else
+					{
+						curSpan = 0;
+					}
 			}
 		}
+		if(curSpan > maxSpan)
+			maxSpan = curSpan;
+		
 		return maxSpan;
 	}
 	
@@ -277,9 +317,9 @@ public class CS2ProgrammingWeek10
 	//	the same number of 4's and 5's, and every 4 has a number after it that is not a 4. In this 
 	//	version, 5's may appear anywhere in the original array. 
 
-	//	follow4with5({5, 4, 9, 4, 9, 5}) → {9, 4, 5, 4, 5, 9}
-	//	follow4with5({1, 4, 1, 5}) → {1, 4, 5, 1}
-	//	follow4with5({1, 4, 1, 5, 5, 4, 1}) → {1, 4, 5, 1, 1, 4, 5}
+	//	follow4with5({5, 4, 9, 4, 9, 5}) == {9, 4, 5, 4, 5, 9}
+	//	follow4with5({1, 4, 1, 5}) == {1, 4, 5, 1}
+	//	follow4with5({1, 4, 1, 5, 5, 4, 1}) == {1, 4, 5, 1, 1, 4, 5}
 	
 	/**
 	 * 
@@ -292,7 +332,7 @@ public class CS2ProgrammingWeek10
 	static int[] follow4with5(int[] nums) 
 	{
 		int j=0;
-		int[] returnArray = new Integer[nums.length];
+		int[] returnArray = new int[nums.length];
 		
 		// copy array
 		for(int i=0; i<nums.length; i++)
@@ -302,22 +342,39 @@ public class CS2ProgrammingWeek10
 		
 		for(int i=0; i<nums.length; i++)
 		{
+			// find next 4
 			if(returnArray[i] == 4 && returnArray[i+1] != 5)
 			{
-				for(int j; j<nums.length; j++)
+				for(int k=j; k<nums.length; k++)
 				{
-					if(returnArray[j] == 5)
-						if(j > 0)
-							if(returnArray[j-1] == 4)
+					// find next 5
+					if(returnArray[k] == 5)
+					{
+						// there are no 5s before this
+						j=k+1;
+						// if a 5 is placed, don't touch this 5
+						if(k > 0)
+						{
+							if(returnArray[k-1] == 4)
 							{
+								
 								continue;
 							}
 							else
 							{
-								returnArray[j] = returnArray[i+1];
+								returnArray[k] = returnArray[i+1];
 								returnArray[i+1] = 5;
 								break;
 							}
+						}
+					
+						else
+						{
+							returnArray[k] = returnArray[i+1];
+							returnArray[i+1] = 5;
+							break;
+						}
+					}
 				}
 			}
 		}
@@ -328,9 +385,9 @@ public class CS2ProgrammingWeek10
 	//	Given n>=0, create an array length n*n with the following pattern, shown here 
 	//	for n=3 : {0, 0, 1,    0, 2, 1,    3, 2, 1} (spaces added to show the 3 groups). 
 
-	//	reverseArithSeries(3) → {0, 0, 1, 0, 2, 1, 3, 2, 1}
-	//	reverseArithSeries(2) → {0, 1, 2, 1}
-	//	reverseArithSeries(4) → {0, 0, 0, 1, 0, 0, 2, 1, 0, 3, 2, 1, 4, 3, 2, 1}
+	//	reverseArithSeries(3) == {0, 0, 1, 0, 2, 1, 3, 2, 1}
+	//	reverseArithSeries(2) == {0, 1, 2, 1}
+	//	reverseArithSeries(4) == {0, 0, 0, 1, 0, 0, 2, 1, 0, 3, 2, 1, 4, 3, 2, 1}
 	
 	/**
 	 * 
@@ -342,7 +399,7 @@ public class CS2ProgrammingWeek10
 	 */
 	static int[] reverseArithSeries(int n) 
 	{
-		int[] series = new Integer[n*n];
+		int[] series = new int[n*n];
 		
 		int sequence = 1;
 		int val = n;
@@ -360,7 +417,7 @@ public class CS2ProgrammingWeek10
 				val--;
 			}
 			
-			if(val < 0)
+			if(val <= 0)
 			{
 				val = n;
 				sequence++;
@@ -373,9 +430,9 @@ public class CS2ProgrammingWeek10
 	//	Say that a "clump" in an array is a series of 2 or more adjacent elements of 
 	//	the same value. Return the number of clumps in the given array. 
 
-	//	largestClump({1, 2, 2, 3, 4, 4}) → 2
-	//	largestClump({1, 1, 2, 1, 1}) → 2
-	//	largestClump({1, 1, 1, 1, 1}) → 1
+	//	largestClump({1, 2, 2, 3, 4, 4}) == 2
+	//	largestClump({1, 1, 2, 1, 1}) == 2
+	//	largestClump({1, 1, 1, 1, 1}) == 1
 	
 	/**
 	 * 
@@ -388,20 +445,25 @@ public class CS2ProgrammingWeek10
 	static int largestClump(int[] nums) 
 	{
 		int count = 0;
+		int prevClump;
+		
 		for(int i=1; i<nums.length; i++)
 		{
 			if(nums[i] == nums[i-1])
 			{
+				prevClump = i;
 				count++;
+				i++;
+				if(i >= nums.length)
+					break;
+			
 				
-				while(i<nums.length)
+				while(nums[i] == nums[prevClump])
 				{
-					if(nums[i] == nums[i-1])
-					{
-						i++;
-					}
+					i++;
+					if(i >= nums.length)
+						break;
 				}
-					
 			}
 		}
 		return count;
@@ -415,6 +477,89 @@ public class CS2ProgrammingWeek10
 	
 	public static void main(String[] args)
 	{
+		int[] array01 = {1, 2, 1, 1, 3};
+		assert maximumSpan(array01) == 4;
+		int[] array02 = {1, 4, 2, 1, 4, 1, 4};
+		assert maximumSpan(array02) == 6;
+		int[] array03 = {1, 4, 2, 1, 4, 4, 4};
+		assert maximumSpan(array03) == 6;
+		
+		int[] array04 = {1, 1, 1, 2, 1};
+		assert canStabilize(array04) == true;
+		int[] array05 = {2, 1, 1, 2, 1};
+		assert canStabilize(array05) == false;
+		int[] array06 = {10, 10};
+		assert canStabilize(array06) == true;
+		
+		printArray(arithmeticSeries(3));// == {1, 1, 2, 1, 2, 3};
+		printArray(arithmeticSeries(4));// == {1, 1, 2, 1, 2, 3, 1, 2, 3, 4};
+		printArray(arithmeticSeries(2));// == {1, 1, 2};
+		System.out.println("");
+		
+		int[] array07 = {1, 3, 1, 4};
+		printArray(follow3with4(array07));// == {1, 3, 4, 1};
+		int[] array08 = {1, 3, 1, 4, 4, 3, 1};
+		printArray(follow3with4(array08));// == {1, 3, 4, 1, 1, 3, 4};
+		int[] array09 = {3, 2, 2, 4};
+		printArray(follow3with4(array09));// == {3, 4, 2, 2};
+		System.out.println("");
+		
+		int[] array10 = {1, 2, 4, 6};
+		int[] array11 = {2, 4};
+		assert innerAppearsInOuter(array10, array11) == true;
+		int[] array21 = {1, 2, 4, 6};
+		int[] array22 = {2, 3, 4};
+		assert innerAppearsInOuter(array21, array22) == false;
+		int[] array23 = {1, 2, 4, 4, 6};
+		int[] array24 = {2, 4};
+		assert innerAppearsInOuter(array23, array24) == true;
+		
+		int[] array12 = {1, 2, 3, 8, 9, 3, 2, 1};
+		assert maximumMirrorSpan(array12) == 3;
+		int[]array13 = {1, 2, 1, 4};
+		assert maximumMirrorSpan(array13) == 3;
+		int[] array14 = {7, 1, 2, 9, 7, 2, 1};
+		assert maximumMirrorSpan(array14) == 2;
+		
+		int[] array15 = {5, 4, 9, 4, 9, 5};
+		printArray(follow4with5(array15));// == {9, 4, 5, 4, 5, 9};
+		int[] array16 = {1, 4, 1, 5};
+		printArray(follow4with5(array16));// == {1, 4, 5, 1};
+		int[] array17 = {1, 4, 1, 5, 5, 4, 1};
+		printArray(follow4with5(array17));// == {1, 4, 5, 1, 1, 4, 5};
+		System.out.println("");
+		
+		printArray(reverseArithSeries(3));// == {0, 0, 1, 0, 2, 1, 3, 2, 1};
+		printArray(reverseArithSeries(2));// == {0, 1, 2, 1};
+		printArray(reverseArithSeries(4));// == {0, 0, 0, 1, 0, 0, 2, 1, 0, 3, 2, 1, 4, 3, 2, 1};
+		System.out.println("");
+		
+		int[] array18 = {1, 2, 2, 3, 4, 4};
+		assert largestClump(array18) == 2;
+		int[] array19 = {1, 1, 2, 1, 1};
+		assert largestClump(array19) == 2;
+		int[] array20 = {1, 1, 1, 1, 1};
+		assert largestClump(array20) == 1;
+		System.out.println("");		
+	}
+	
+	public static void printArray(int[] array)
+	{
+		for(int i=0; i<array.length; i++)
+		{
+			System.out.print(array[i] + ", ");
+		}
+		System.out.println("");
+	}
+	
+	public void printNum(int num)
+	{
+		System.out.println(num);
+	}
+	
+	public void printBool(boolean bool)
+	{
+		System.out.println(bool);
 	}
 	
 }
